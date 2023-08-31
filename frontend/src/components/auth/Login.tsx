22ac1428fa5c18246ba82f4cc4d98dbec0291126
Remerson Monteiro <kairemerson@gmail.com>
2023-08-30 22:42:54 -0300

    modified login and signup to use formik and yup

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,57 +1,70 @@
 import styles from "./styles/login.module.css"
-import { ChangeEvent, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import * as yup from "yup"
+import { useFormik} from "formik"
 import 'react-toastify/dist/ReactToastify.css';
 import { api } from "../../services/api"
 
  export const Login = ()=>{
 
-    const [email, setEmail] = useState("")
-    const[password, setPassword] = useState("")
     const navigate = useNavigate()
 
     const loginSchema = yup.object().shape({
-        email: yup.string().email("email inválido").required("email é obrigatório"),
-        password: yup.string().required().min(6,"A senha deve ter mais de 6 dígitos")
+        email: yup.string().email("Email inválido").required("O email é obrigatório"),
+        password: yup.string().required("A senha é obrigatória").min(6,"A senha deve ter mais de 6 dígitos")
     })
 
-    const handleSubmit = async ()=>{
-        
-        await api.post("/oapi/login", {email, password})
-            .then((data)=>{
-                toast.success(data.data.success)
-                setEmail("")
-                setPassword("")
-                navigate("/home")
-
-            }).catch((err)=>{
-                toast.error(err.response.data.error)
-            })
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: loginSchema,
+        onSubmit: async (values, actions) =>{
             
-    }
+            await api.post("/oapi/login", values)
+                .then((resp)=>{
+                    toast.success(resp.data.success)
+                    navigate("/home")
+                    
+                }).catch((err)=>{
+                    toast.error(err.response.data.error)
+                    actions.resetForm()
+                   
+                })
+        }
+    })
+
+    
     return(
         <div className={styles.wrapper}>
-            <div className={styles.box_login}>
-                <h2 className={styles.title}>Login</h2>
-                <label htmlFor="email">E-mail:</label>
-                <input id="email" 
-                    className={styles.input_login} 
-                    type="text" 
-                    placeholder="Insira o email"
-                    value={email}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setEmail(e.target.value)}}/>
-                <label htmlFor="password">Senha:</label>
-                <input id="password" 
-                    className={styles.input_login} 
-                    type="text" 
-                    placeholder="Insira a senha"
-                    value={password}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setPassword(e.target.value)}}/>
-                <button className={styles.btn_submit} onClick={handleSubmit}>Entrar</button>
-                <p>Não tem uma conta? <span className={styles.create_acc} onClick={()=>{navigate("/signup")}}>Inscreva-se</span></p>
-            </div>
+                <form onSubmit={formik.handleSubmit} className={styles.box_login} >
+                    <h2 className={styles.title}>Login</h2>
+                    <label htmlFor="email" className={styles.label}>E-mail:</label>
+                    <input id="email"
+                        name="email" 
+                        className={`${styles.input_login} ${formik.errors.email && formik.touched.email ? styles.input_error : ""}`}
+                        type="text" 
+                        placeholder="Insira o email"
+                        value={formik.values.email}
+                        onBlur={formik.handleBlur}
+                        onChange={formik.handleChange}/>
+                    {formik.errors.email ? <span className={styles.error}>{formik.errors.email}</span> : null}
+                    <label htmlFor="password" className={styles.label}>Senha:</label>
+                    <input id="password"
+                        name="password" 
+                        className={`${styles.input_login} ${formik.errors.password ? styles.input_error : ""}`} 
+                        type="password" 
+                        placeholder="Insira a senha"
+                        onBlur={formik.handleBlur}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}/>
+                    {formik.errors.password ? <span className={styles.error}>{formik.errors.password}</span> : null}
+                    <button className={styles.btn_submit} type="submit">Entrar</button>
+
+                    <p>Não tem uma conta? <span className={styles.create_acc} onClick={()=>{navigate("/signup")}}>Inscreva-se</span></p>
+                </form>
         </div>
     )
 }
--- frontend/src/components/auth/Signup.tsx
+++ frontend/src/components/auth/Signup.tsx
@@ -2,63 +2,95 @@ import styles from "./styles/login.module.css"
 import { ChangeEvent, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
+import {useFormik} from "formik"
+import * as yup from "yup"
 import 'react-toastify/dist/ReactToastify.css';
 import {api} from "../../services/api"
 
 export const Signup = ()=> {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
     const navigate = useNavigate()
-    const handleSignup = async ()=>{
-        await api.post("/oapi/signup",{name, email, password, confirmPassword})
-        .then((data)=>{
-            toast.success(data.data.success)
-            console.log(data.data);
-            setName("")
-            setEmail("")
-            setPassword("")
-            setConfirmPassword("")
-            navigate("/")
-            
-        })
-        .catch((err)=>{
-            console.log(err.response.data);
-            toast.error(err.response.data.error)
-        })
-    }
+
+    const signupSchema = yup.object().shape({
+        name: yup.string().required("O nome é obrigatório").min(3, "O nome deve ter mais de 3 caracteres"),
+        email: yup.string().email("Email inválido").required("O email é obrigatório"),
+        password: yup.string().min(6, "A senha deve ter mais de 6 dígitos").required("A senha é obrigatória"),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), ""],"A confirmação de senha deve ser igual a senha").required("A confirmação de senha é obrigatória")
+
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        },
+        validationSchema: signupSchema,
+        onSubmit: async (values, actions)=> {
+            await api.post("/oapi/signup", values)
+            .then((resp)=>{
+                toast.success(resp.data.success)
+                navigate("/")
+                
+            })
+            .catch((err)=>{
+                console.log(err.response.data);
+                toast.error(err.response.data.error)
+                actions.resetForm()
+            })
+        }
+    })
 
     return(
         <div className={styles.wrapper}>
-            <div className={styles.box_login}>
+            <form className={styles.box_login} onSubmit={formik.handleSubmit}>
                 <h2 className={styles.title}>Criar conta</h2>
-                <label htmlFor="">Nome:</label>
-                <input type="text" 
-                    className={styles.input_login}
+                <label htmlFor="name" className={styles.label}>Nome:</label>
+                <input type="text"
+                    name="name"
+                    id="name"
+                    className={`${styles.input_login} ${formik.errors.name && formik.touched.name ? styles.input_error : ""}`}
                     placeholder="Insira o nome"
-                    value={name}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setName(e.target.value)}}/>
-                <label htmlFor="">E-mail:</label>
-                <input type="text" 
-                    className={styles.input_login}
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}/>
+                {formik.errors.name ? <span className={styles.error}>{formik.errors.name}</span> : null}
+
+                <label htmlFor="email" className={styles.label}>E-mail:</label>
+                <input type="text"
+                    id="email"
+                    name="email" 
+                    className={`${styles.input_login} ${formik.errors.email && formik.touched.email ? styles.input_error : ""}`}
                     placeholder="Insira o email"
-                    value={email}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setEmail(e.target.value)}}/>
-                <label htmlFor="">Senha:</label>
-                <input type="text" 
-                    className={styles.input_login}
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}/>
+                {formik.errors.email ? <span className={styles.error}>{formik.errors.email}</span> : null}
+
+                <label htmlFor="password" className={styles.label}>Senha:</label>
+                <input type="password" 
+                    id="password"
+                    name="password"
+                    className={`${styles.input_login} ${formik.errors.password && formik.touched.password ? styles.input_error : ""}`}
                     placeholder="Insira a senha"
-                    value={password}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setPassword(e.target.value)}}/>
-                <label htmlFor="">Confirme a senha:</label>
-                <input type="text" 
-                    className={styles.input_login}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}/>
+                {formik.errors.password ? <span className={styles.error}>{formik.errors.password}</span> : null}
+
+                <label htmlFor="confirmPassword" className={styles.label}>Confirme a senha:</label>
+                <input type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    className={`${styles.input_login} ${formik.errors.confirmPassword && formik.touched.confirmPassword ? styles.input_error : ""}`}
                     placeholder="Insira a senha"
-                    value={confirmPassword}
-                    onChange={(e: ChangeEvent<HTMLInputElement>)=>{setConfirmPassword(e.target.value)}}/>
-                <button className={styles.btn_submit} onClick={handleSignup}>Enviar</button>
-            </div>
+                    value={formik.values.confirmPassword}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}/>
+                {formik.errors.confirmPassword ? <span className={styles.error}>{formik.errors.confirmPassword}</span> : null}
+
+                <button className={styles.btn_submit} type="submit">Enviar</button>
+            </form>
         </div>
     )
 }
